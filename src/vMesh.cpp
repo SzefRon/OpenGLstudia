140999363cb33de4aa9029eb643663b12c218d93
SzefRon <92161300+SzefRon@users.noreply.github.com>
2023-11-26 04:13:22 +0100

    formatting

--- src/vMesh.cpp
+++ src/vMesh.cpp
@@ -38,15 +38,14 @@ void vMesh::draw(Shader &shader)
 {
     unsigned int diffuseNr = 1;
     unsigned int specularNr = 1;
-    for(unsigned int i = 0; i < textures.size(); i++)
-    {
+    for (unsigned int i = 0; i < textures.size(); i++) {
         glActiveTexture(GL_TEXTURE0 + i); // activate proper texture unit before binding
         // retrieve texture number (the N in diffuse_textureN)
         std::string number;
         std::string name = textures[i].type;
-        if(name == "texture_diffuse")
+        if (name == "texture_diffuse")
             number = std::to_string(diffuseNr++);
-        else if(name == "texture_specular")
+        else if (name == "texture_specular")
             number = std::to_string(specularNr++);
 
         shader.setUInt(("material." + name + number).c_str(), i);
